210e24740844ddf86f15c5ec9f5d44f86d5e2fdb
Byeong Min Cho <merrybmc@gmail.com>
2023-11-17 20:04:41 +0900

    error 컴포넌트

---
+++ note/src/app/products/error.tsx
@@ -0,0 +1,19 @@
+'use client';
+import { useEffect } from 'react';
+
+interface Props {
+  error: Error;
+  reset: () => void;
+}
+
+export default function ProductsError({ error, reset }: Props) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button onClick={() => reset()}>Try again</button>
+    </div>
+  );
+}
