26ade9c1d404fd9b0f08c0017800617cf85e679d
Byeong Min Cho <merrybmc@gmail.com>
2023-11-17 21:18:47 +0900

    폰트 사용

--- note/src/app/layout.tsx
+++ note/src/app/layout.tsx
@@ -3,8 +3,15 @@ import { Inter } from 'next/font/google';
 // import './globals.css';
 import styles from './layout.module.css';
 import Link from 'next/link';
+import { Open_Sans } from 'next/font/google';
+import { Nanum_Gothic } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+// const inter = Inter({ subsets: ['latin'] });
+const sans = Open_Sans({ subsets: ['latin'] });
+const gothic = Nanum_Gothic({
+  weight: '400',
+  subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
   title: '멋진 제품 사이트',
@@ -13,11 +20,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
+    <html lang='en' className={sans.className}>
       {/* <body className={inter.className}>{children}</body> */}
       <body className={styles.body}>
         <header className={styles.header}>
-          <h1>Demo Note App</h1>
+          <h1 className={gothic.className}>Demo Note App</h1>
           <nav className={styles.nav}>
             <Link href='/'>Home</Link>
             <Link href='/products'>Product</Link>
