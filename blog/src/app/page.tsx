23f191abe8de91f67f7190ea6f5d87ecf79ab42d
Byeong Min Cho <merrybmc@gmail.com>
2023-11-20 21:28:38 +0900

    home profile 작업하기

--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -1,8 +1,28 @@
 import { Open_Sans } from 'next/font/google';
+import Image from 'next/image';
 import Link from 'next/link';
+import pikachu from '../../public/hello-pikachu.gif';
 
 const sans = Open_Sans({ subsets: ['latin'] });
 
 export default function Home() {
-  return <section>Main</section>;
+  return (
+    <section className='flex flex-col items-center justify-center'>
+      <Image
+        src={pikachu}
+        alt='pikachu'
+        className='mt-8 mb-4 rounded-full'
+        width={350}
+        height={350}
+        priority
+      />
+      <h2 className='text-2xl font-bold'>{"Hi I'm Byeong Min"}</h2>
+      <h3 className='text-xl font-medium'>Front-end Enginner</h3>
+      <Link href='/contact'>
+        <button className='px-4 py-1.5 mt-4 font-semibold rounded-3xl bg-orange-400'>
+          Contact Me!
+        </button>
+      </Link>
+    </section>
+  );
 }
